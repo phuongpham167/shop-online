fe6eee8f9e47848acfd6fa4a339365b8f7cfbcf7
phuongpham167 <phuongptt.uet@gmail.com>
2019-01-23 10:59:16 +0700

    learn-day2

---
+++ resources/views/simple.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hi</title>
+</head>
+<body>
+	<p>This is bo di :3</p>
+	<?php echo $test; ?>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,39 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return View::make('simple');
+});
+
+Route::get('my/page', function () { 
+   // return 'Hello world!';
+   return Redirect::to('my/test');
+});
+
+// Route::get();
+// Route::post();
+// Route::put();
+// Route::patch();
+// Route::delete();
+// Route::any();
+
+$logic = function() {
+	return Response::make('Hello world!', 200);
+};
+Route::get('my/test', $logic);
+
+//routes parameter
+Route::get('/books/{genre?}', function($genre = null){
+	if ($genre == null) {
+		return 'Genre = null';
+	}
+	else{
+		return 'Genre is {$genre}';
+	}
+});
+
+Route::get('/test2/{test}', function($test){
+	$data['test'] = $test;
+
+	return View::make('simple', $data);
 });
