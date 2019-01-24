b80086ce6868c408337df6e9b19e771fb9cee5bc
phuongpham167 <phuongptt.uet@gmail.com>
2019-01-24 15:03:38 +0700

    learn day 3

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    // return view('welcome');
-    return View::make('simple');
-});
+// Route::get('/', function () {
+//     // return view('welcome');
+//     return View::make('simple');
+// });
 
 Route::get('my/page', function () { 
    // return 'Hello world!';
@@ -66,6 +66,55 @@ Route::get('file/download', function(){
 	$filename = 'abc.pdf';
 	return Response::download($filename);
 });
+
+Route::get('/request_data', function(){
+	$data = Request::all();
+	var_dump($data);
+});
+
+Route::get('/', function(){
+	// $data = Request::all();
+	$data = Request::get('foo', 'bar'); //neu tren url gui len co foo thi se in ra gia tri cua foo neu khong thi in ra gia tri mac dinh cua foo da dc gan la bar
+	$data2 = Request::has('foo'); //kiem tra request gui len co foo ko va tra ve gia tri boolean la true or false
+
+	var_dump($data2);
+});
+
+Route::get('post-form', function(){
+	return View::make('form');
+});
+
+//Cookie
+Route::get('/cookie-babie', function(){
+	$cookie = Cookie::make('low-carb', 'almond cookie', 30);
+	return Response::make('Nom nom.')->withCookie($cookie);
+});
+
+Route::get('/nom-nom', function(){
+	$cookie = Cookie::get('low-carb');
+	var_dump($cookie);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
